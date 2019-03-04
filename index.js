45ee9eba68a4ab171f72f474d6120e825b2f5f2f
ChelseaTolnai <ChelseaTolnai@gmail.com>
2019-03-04 10:25:48 -0500

    Completed auth 1 guided project.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
+const bcrypt = require('bcryptjs')
 
 const db = require('./database/dbConfig.js');
 const Users = require('./users/users-module.js');
@@ -18,8 +19,9 @@ server.get('/', (req, res) => {
 server.post('/api/register', (req, res) => {
   let user = req.body;
   // generate hash from user's password
-  
+  const hash = bcrypt.hashSync(user.password, 16); // 2^n
   // override user.password with hash
+  user.password = hash;
 
   Users.add(user)
     .then(saved => {
@@ -37,7 +39,7 @@ server.post('/api/login', (req, res) => {
     .first()
     .then(user => {
       // check that passwords match
-      if (user) {
+      if (user && bcrypt.compareSync(password, user.password)) {
         res.status(200).json({ message: `Welcome ${user.username}!` });
       } else {
         res.status(401).json({ message: 'Invalid Credentials' });
@@ -48,8 +50,29 @@ server.post('/api/login', (req, res) => {
     });
 });
 
+function restricted(req, res, next) {
+  const { username, password } = req.headers;
+
+  if(username && password) {
+    Users.findBy({ username })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        next();
+      } else {
+        res.status(401).json({ message: 'Invalid Credentials' });
+      }
+    })
+    .catch(error => {
+      res.status(500).json({ message: 'Ran into an unexpected error' });
+    });
+  } else {
+    res.status(500).json({ message: 'No Credentials provided' });
+  }
+}
+
 // protect this route, only authenticated users should see it
-server.get('/api/users', (req, res) => {
+server.get('/api/users', restricted, (req, res) => {
   Users.find()
     .then(users => {
       res.json(users);
