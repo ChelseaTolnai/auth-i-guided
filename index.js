11f752084e1d11736c69d4949cdd8d6b79ed79db
ChelseaTolnai <ChelseaTolnai@gmail.com>
2019-03-04 19:40:37 -0500

    Completed Day 1: Auth 2 guided project.

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
+require('dotenv').config();
+
 const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
-const bcrypt = require('bcryptjs')
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken'); // 1 - Add jwt after yarn install
 
 const db = require('./database/dbConfig.js');
 const Users = require('./users/users-module.js');
+
+const secret = process.env.JWT_SECRET || 'add a third table for many to many relationships' // 4 - Define secret
 
 const server = express();
 
@@ -18,9 +23,7 @@ server.get('/', (req, res) => {
 
 server.post('/api/register', (req, res) => {
   let user = req.body;
-  // generate hash from user's password
-  const hash = bcrypt.hashSync(user.password, 16); // 2^n
-  // override user.password with hash
+  const hash = bcrypt.hashSync(user.password, 6);
   user.password = hash;
 
   Users.add(user)
@@ -32,15 +35,36 @@ server.post('/api/register', (req, res) => {
     });
 });
 
+
+// 3 - Define generate token function
+function generateToken(user) {
+    const payload = {
+        subject: user.id, // sub in payload is what the token is about
+        username: user.username,
+        roles: ['Student'],
+        // ... any other data
+    }
+
+    const options = {
+        expiresIn: '1d',
+    }
+
+    return jwt.sign(payload, secret, options);
+}
+
 server.post('/api/login', (req, res) => {
   let { username, password } = req.body;
 
   Users.findBy({ username })
     .first()
     .then(user => {
-      // check that passwords match
       if (user && bcrypt.compareSync(password, user.password)) {
-        res.status(200).json({ message: `Welcome ${user.username}!` });
+        const token = generateToken(user); // 2 - create token
+        res.status(200).json({ 
+            message: `Welcome ${user.username}! Have a token.`, 
+            token,
+            roles: token.roles,
+        });
       } else {
         res.status(401).json({ message: 'Invalid Credentials' });
       }
@@ -50,35 +74,43 @@ server.post('/api/login', (req, res) => {
     });
 });
 
+// 5 - create restricted middleware
 function restricted(req, res, next) {
-  const { username, password } = req.headers;
+    const token = req.headers.authorization;
 
-  if(username && password) {
-    Users.findBy({ username })
-    .first()
-    .then(user => {
-      if (user && bcrypt.compareSync(password, user.password)) {
-        next();
-      } else {
-        res.status(401).json({ message: 'Invalid Credentials' });
-      }
-    })
-    .catch(error => {
-      res.status(500).json({ message: 'Ran into an unexpected error' });
-    });
-  } else {
-    res.status(500).json({ message: 'No Credentials provided' });
-  }
+    if(token) {
+        //is it valid?
+        jwt.verify(token, secret, (err, decodedToken) => {
+            if(err) {
+                res.status(401).json( {you: 'can\'t touch this'})
+            } else {
+                req.decodedJwt = decodedToken;
+                next();
+            }
+        })
+    } else {
+        res.status(401).json( {you: 'shall not pass!'})
+    }
 }
 
-// protect this route, only authenticated users should see it
-server.get('/api/users', restricted, (req, res) => {
+function checkRole(role) {
+    return function(req, res, next) {
+        if(req.decodedJwt.roles && req.decodedJwt.roles.includes(role)) {
+            next();
+        } else {
+            res.status(403).json({ you: 'have no power here'})
+        }
+    }
+}
+
+server.get('/api/users', restricted, checkRole('Student'), (req, res) => {
   Users.find()
     .then(users => {
-      res.json(users);
+      res.json({ users, decodedToken: req.decodedJwt });
     })
     .catch(err => res.send(err));
 });
+
 
 const port = process.env.PORT || 5000;
 server.listen(port, () => console.log(`\n** Running on port ${port} **\n`));
